fd860d6fd51948df1f57316707ad0337f2202599
ElenaChudinova <maknali@yandex.ru>
2023-09-19 19:20:52 +0300

    different cod

--- str_func.py
+++ str_func.py
@@ -1,6 +1,7 @@
 def title_alpha():
     '''Строка с заглавной буквы'''
     user = input()
-    return ' '.join(word[0].title + wird[1:].lower for word in s.split())
+    return ' '.join(word[0].title + word[1:].lower for word in user.split())
 
 
+print(title_alpha())
