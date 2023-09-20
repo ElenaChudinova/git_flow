9c6dfdb99e629cd205a1ab809c55911e52ab1143
Elena Chudinova <maknali@yandex.ru>
2023-09-20 20:57:48 +0300

    Решаю конфликт2

--- str_func1.py
+++ str_func1.py
@@ -1,7 +1,6 @@
-def title_alpha():
+def title_user():
     '''Строка с заглавной буквы'''
-    user = input()
-    return ' '.join(word[0].title + word[1:].lower for word in user.split())
+    return input().title
 
 
 
